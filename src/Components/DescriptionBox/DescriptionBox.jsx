7572a88bcd1d1013f55e0c6e7dcbd81a5ef56484
PechettiCharan <charanpechetti@gmail.com>
2025-10-15 12:35:12 +0530

    commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './DescriptionBox.css'
+export const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">
+                Description
+            </div>
+            <div className="descriptionbox-nav-box fade">
+                Reviews (122)
+            </div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>asdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
+                     hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvbasdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
+                     hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvbasdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
+                     hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvbasdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
+                     hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvb
+                </p>
+                <p>asdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
+                hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvbasdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
+                hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvbasdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
+                hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvbasdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
+                hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvb</p>
+            </div>
+    </div>
+  )
+}
