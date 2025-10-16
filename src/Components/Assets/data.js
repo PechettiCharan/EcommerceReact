14bda907195ae001b098e4eacffb8f970afc5a7b
PechettiCharan <charanpechetti@gmail.com>
2025-10-16 13:03:08 +0530

    solution

--- src/Components/Assets/data.js
+++ src/Components/Assets/data.js
@@ -1,6 +1,6 @@
 import p1_img from './product_1.png'
-import p2_img from './product_2.png'
-import p3_img from './product_3.png'
+import p2_img from './product_13.png'
+import p3_img from './product_30.png'
 import p4_img from './product_4.png'
 
 let data_product = [
@@ -12,13 +12,13 @@ let data_product = [
     old_price:80.50,
   },
   {id:2,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
     image:p2_img,
     new_price:85.00,
     old_price:120.50,
   },
   {id:3,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Boys Orange Colourblocked Hooded Sweatshirt",
     image:p3_img,
     new_price:60.00,
     old_price:100.50,
