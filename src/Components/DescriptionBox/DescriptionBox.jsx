bc2a080345632b1b548ef7d9d6ed3865332cffe2
PechettiCharan <charanpechetti@gmail.com>
2025-10-16 12:47:59 +0530

    Updated solution

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -12,17 +12,9 @@ export const DescriptionBox = () => {
             </div>
             </div>
             <div className="descriptionbox-description">
-                <p>asdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
-                     hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvbasdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
-                     hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvbasdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
-                     hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvbasdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
-                     hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvb
+                <p>An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products, interact with customers, and conduct transactions without the need for a physical presence. E-commerce websites have gained immense popularity due to their convenience, accessibility, and the global reach they offer.
                 </p>
-                <p>asdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
-                hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvbasdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
-                hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvbasdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
-                hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvbasdfghjkloiutcghjkij7trfghjnkok vhjy hjiju ju u bhhb bhbhhb hbbh bh bhhb hb hbhb hb hbhb bh bh
-                hh bj k nk jk j jidfghjkliuytrfvbnm,liuytfvb</p>
+                <p>E-commerce websites typically display products or services along with detailed descriptions, images, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with relevant information.</p>
             </div>
     </div>
   )
--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -33,7 +33,7 @@ export const ProductDisplay = (props) => {
                     <div className="productdisplay-right-prices-old">${product.old_price}</div>
                     <div className="productdisplay-right-prices-new">${product.new_price}</div>
                 </div>
-                <div className="productdisplay-right-description">A lightweight fghjkl ghjkl ghjkvgh jmk,vgbhnmvbhjkghjkghjklbnhj j jj jk kkj .
+                <div className="productdisplay-right-description">A lightweight usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.
 
                 </div>
                 <div className="productdisplay-right-size">
